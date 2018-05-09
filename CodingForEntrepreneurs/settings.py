e479ab71994e06ca1f0705bfeffe6c911666b016
tahirs95 <tahirs95@hotmail.com>
2018-05-10 01:33:25 +0500

    Second Commit

--- CodingForEntrepreneurs/settings.py
+++ CodingForEntrepreneurs/settings.py
@@ -148,7 +148,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
