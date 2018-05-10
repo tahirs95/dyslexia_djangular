a56185c0d97d5ff8d35d162eba7b4a15dbb6bb10
tahirs95 <tahirs95@hotmail.com>
2018-05-11 00:25:38 +0500

    Changes in settings

--- CodingForEntrepreneurs/settings.py
+++ CodingForEntrepreneurs/settings.py
@@ -156,9 +156,9 @@ DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
